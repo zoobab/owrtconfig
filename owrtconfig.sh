ec18ae1817b7d776f087561f313cd7478beabb49
Nicolas Thill <nicolas.thill@gmail.com>
2010-06-03 12:31:55 +0200

    cleanup, fix IFS issues on MacOSX, bump to v0.04

--- owrtconfig.sh
+++ owrtconfig.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 ME="owrtconfig.sh"
-VER="0.03"
-IP="192.168.1.1"
-SKIP_LINE_COUNT=0
+VER="0.04"
 
 _error() {
 	echo "$ME: $*"
@@ -17,7 +15,7 @@ $ME v$VER
 Usage: $ME OPTIONS -H HOSTS -C COMMANDS
 
   -P PROTOCOL   protocol used to connect on each node (ssh or telnet, default: ssh)
-  -H HOSTS      file containing hosts list (hwaddr,param1,param2...param9))
+  -H HOSTS      file containing hosts list (hwaddr,param1,param2...param9)
   -C COMMANDS   file containing commands to be run on each host
   -a ADDRESS    default IP address of each host (default: 192.168.1.1)
   -n LINES      lines to skip from remote output (default: 0)
@@ -36,11 +34,25 @@ __END_OF_VERSION
 }
 
 _parse_args() {
+	IP="192.168.1.1"
+	PROTOCOL_FUNC="_ssh"
+	SKIP_LINE_COUNT=0
 	while [ -n "$1" ]; do
 		case $1 in
 		  -P|--protocol)
 			shift
-			PROTOCOL="$1"
+			case $1 in
+			  ssh)
+				PROTOCOL_FUNC="_ssh"
+				;;
+			  telnet)
+				PROTOCOL_FUNC="_telnet"
+				;;
+			  *)
+				_error "unknow protocol '$1'"
+				;;
+			esac
+			
 			;;
 		  -H|--hosts)
 			shift
@@ -70,7 +82,7 @@ _parse_args() {
 			exit 0
 			;;
 		  *)
-			_error "unexpected argument"
+			_error "unexpected argument '$1'"
 			;;
 		esac
 		shift
@@ -79,8 +91,6 @@ _parse_args() {
 
 _parse_args $*
 
-[ -n "$PROTOCOL" ] \
-	|| _error "missing '-P PROTOCOL' argument"
 [ -n "$HOSTS_F" ] \
 	|| _error "missing '-H HOSTS' argument"
 [ -n "$COMMANDS_F" ] \
@@ -100,44 +110,42 @@ _telnet( )
 	nc -i 1 -t "$IP" 23 2>&1
 }
 
-case $PROTOCOL in
-  ssh)
-	PROTOCOL_FUNC="_ssh"
-	;;
-  telnet)
-	PROTOCOL_FUNC="_telnet"
-	;;
-esac
+if [ -n "$SUDO_FUNC" ]; then
+	echo "** checking sudo..."
+	$SUDO_FUNC true || exit 1
+fi
 
-echo "1. checking sudo..."
-sudo true || exit 1
-
-echo "2. looping over nodes..."
-IFS=","
+echo "** looping over nodes..."
+IFS_OLD="$IFS"
+IFS_NEW=","
+IFS="$IFS_NEW"
 cat $HOSTS_F | grep -v '^#' |  sed -e 's/ *, */,/g' -e's/\//###/g' -e 's/\&\&/####/g'  | while read mac param1 param2 param3 param4 param5 param6 param7 param8 param9; do
-  echo -n "-- mac: $mac -- " 1>&2
-  sudo arp -s $IP $mac >/dev/null
-  ping -c 1 -q -r -t 1 $IP >/dev/null
-  if [ $? -eq 0 ]; then
-    echo "alive! ---" 1>&2
-    echo "[$mac] [$param1] [$param2] [$param3] [$param4] [$param5] [$param6] [$param7] [$param8] [$param9]"
-    cat $COMMANDS_F \
-	| sed	-e "s/@MAC@/$mac/g" \
-		-e "s/@PARAM1@/$param1/g" \
-		-e "s/@PARAM2@/$param2/g" \
-		-e "s/@PARAM3@/$param3/g" \
-		-e "s/@PARAM4@/$param4/g" \
-		-e "s/@PARAM5@/$param5/g" \
-		-e "s/@PARAM6@/$param6/g" \
-		-e "s/@PARAM7@/$param7/g" \
-		-e "s/@PARAM8@/$param8/g" \
-		-e "s/@PARAM9@/$param9/g" \
-		-e "s/####/\&\&/g" \
-		-e "s/###/\//g" \
-        | $PROTOCOL_FUNC | tail -n +$SKIP_LINE_COUNT
-  else
-    echo "not found! ---" 1>&2
-  fi
-  sudo arp -d $IP >/dev/null
+	IFS="$IFS_OLD"
+	echo -n "-- mac: $mac -- " 1>&2
+	$SUDO_FUNC arp -s $IP $mac >/dev/null
+	ping -c 1 -q -r -t 1 $IP >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "alive! --" 1>&2
+		echo "[$mac] [$param1] [$param2] [$param3] [$param4] [$param5] [$param6] [$param7] [$param8] [$param9]"
+		cat $COMMANDS_F \
+			| sed -e "s/@MAC@/$mac/g" \
+			      -e "s/@PARAM1@/$param1/g" \
+			      -e "s/@PARAM2@/$param2/g" \
+			      -e "s/@PARAM3@/$param3/g" \
+			      -e "s/@PARAM4@/$param4/g" \
+			      -e "s/@PARAM5@/$param5/g" \
+			      -e "s/@PARAM6@/$param6/g" \
+			      -e "s/@PARAM7@/$param7/g" \
+			      -e "s/@PARAM8@/$param8/g" \
+			      -e "s/@PARAM9@/$param9/g" \
+			      -e "s/####/\&\&/g" \
+			      -e "s/###/\//g" \
+		| $PROTOCOL_FUNC | tail -n +$SKIP_LINE_COUNT
+	else
+		echo "not found! ---" 1>&2
+	fi
+	$SUDO_FUNC arp -d $IP >/dev/null
+	IFS="$IFS_NEW"
 done
-echo "3. done!"
+IFS="$IFS_OLD"
+echo "** done!"
